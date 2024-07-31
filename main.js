a5ad41b80b570d54a1dee397661825f486702306
SGWinter <seangcalgary@gmail.com>
2024-07-31 11:24:31 -0600

    added argv errors and messages to main

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 function main() {
-    // ?
+    const argv = process.argv
+    if (argv.length < 3) {
+        console.error(`Please provide an argument`)
+    }
+    if (argv.length > 3) {
+        console.error(`Too many arguments provided`)
+    }
+    if (argv.length === 3) {
+        console.log(`Crawler is starting at ${argv[2]}`)
+    }
 }
 
 main()
